edd12fb95e425369378a4a6bd03c6235c4c5b8fb
Ming Xiang <mingxiangchan@gmail.com>
2015-10-16 11:33:04 +0800

    completed users signup

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -4,7 +4,7 @@ module StaticPagesHelper
 		if page_title.empty?
 			site_title
 		else
-			"#{site_title} | #{page_title}"
+			"#{site_title} | #{page_title}".html_safe #use html_safe to escape apostrophe
 		end
 	end
 end
