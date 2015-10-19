9e84035e13599ba0681af765dad7957fbd0bf8bf
Ming Xiang <mingxiangchan@gmail.com>
2015-10-19 12:36:07 +0800

    completed basic form

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,18 @@
 class ListingsController < ApplicationController
+  def show
+  end
+
+  def new
+  	@listing = Listing.new
+  	@countries = Country.all
+  end
+
+  def create
+  	@listing = Listing.new(params[listing_params])
+  end
+
+  private
+  	def listing_params
+  		params.require(:listing).permit(:title, :description, :max_guests, :country_id)
+  	end
 end
