d7b41818a4dfe570131a4f1c6fd991012650400a
Ming Xiang <mingxiangchan@gmail.com>
2015-10-19 23:53:19 +0800

    completed passing variables to js

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
 		minDate: 0,
 		maxDate: 60
 	});
+
+	console.log($('.dates').data('dates'))
+	$( "#start-date-picker" ).datepicker({
+	});
+
+	$( "#end-date-picker" ).datepicker({
+	});
 });
