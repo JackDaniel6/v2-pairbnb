4360578e25fb0a60e1a4c9c15ad1aacf90174b6f
Ming Xiang <mingxiangchan@gmail.com>
2015-10-20 10:18:24 +0800

    completed reservation creation process

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,19 @@
 class ReservationsController < ApplicationController
-	def new
-		
+	def create
+		input = reservation_params
+		input[:start_date] = Date.strptime(input[:start_date], '%m-%d-%Y')
+		input[:end_date] = Date.strptime(input[:end_date], '%m-%d-%Y')
+		byebug
+		@reservation = Reservation.new(input)
+		@reservation.user_id = current_user.id if current_user
+		debugger
+		if @reservation.save
+			redirect_to Listing.find(input[:listing_id])
+		end
 	end
+
+  private
+  	def reservation_params
+  		params.require(:reservation).permit(:start_date, :end_date, :num_guests, :listing_id)
+  	end
 end
--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -4,4 +4,5 @@ module ReservationsHelper
 	def reservation_exists?(listing_id)
 		current_user.reservations.find_by(listing_id: listing_id).nil?
 	end
+
 end
