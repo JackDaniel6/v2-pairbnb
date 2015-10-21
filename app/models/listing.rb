42ed22f10a6b9d0dc1e44986dda3f37a00848839
Ming Xiang <mingxiangchan@gmail.com>
2015-10-21 16:29:17 +0800

    fixed listings index bug

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -19,7 +19,7 @@ class Listing < ActiveRecord::Base
 	end
 
 	def self.search(title, country_id)
-		return Listing.all.order('created_at DESC').limit(20) if title.blank? && country_id==0
+		return Listing.all.order('created_at DESC').limit(20) if title.blank? && (country_id==0 || country_id.nil? )
 	    if (!title.blank? || country_id != 0)
 	      if (!title.blank? && country_id != 0)
 	        return Listing.search_by_both(title, country_id).order("created_at DESC")
